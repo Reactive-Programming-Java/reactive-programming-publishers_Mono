a106acac96d5b89421fd00ed85adfb0af537a30a
Elimane Fofana <elimane.fofana@supinfo.com>
2022-11-03 03:57:47 +0100

    [MONO] Subscribe. Closes #3

---
+++ src/main/java/com/rp/MonoSubscribe.java
@@ -0,0 +1,21 @@
+package com.rp;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * @PROJECT Mono_Project
+ * @Author Elimane on 03/11/2022
+ */
+public class MonoSubscribe {
+  public static void main(String[] args) {
+    // Publisher
+    Mono<String> mono = Mono.just("John");
+
+    // Using onNext (First parameter) to push data to subscriber
+    mono.subscribe(
+      item -> System.out.println(item), // OnNext
+      err -> System.out.println(err), // OnError
+      () -> System.out.println("Completed") // onComplete
+    );
+  }
+}
